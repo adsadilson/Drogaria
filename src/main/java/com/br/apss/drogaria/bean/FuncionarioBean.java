9fdfedcb5e28ee8921be969d8098a76e8342b099
ads <ads@paraguai>
2018-01-12 21:57:22 -0300

    Tela Funcionario

---
+++ src/main/java/com/br/apss/drogaria/bean/FuncionarioBean.java
@@ -0,0 +1,173 @@
+package com.br.apss.drogaria.bean;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.omnifaces.util.Messages;
+import org.primefaces.context.RequestContext;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+
+import com.br.apss.drogaria.enums.Estado;
+import com.br.apss.drogaria.enums.EstadoCivil;
+import com.br.apss.drogaria.enums.Sexo;
+import com.br.apss.drogaria.enums.TipoPessoa;
+import com.br.apss.drogaria.model.Pessoa;
+import com.br.apss.drogaria.model.filter.PessoaFilter;
+import com.br.apss.drogaria.service.PessoaService;
+import com.br.apss.drogaria.util.jsf.NegocioException;
+
+@Named
+@ViewScoped
+public class FuncionarioBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Pessoa funcionario = new Pessoa();
+
+	private Pessoa funcionarioSelecionado;
+
+	private PessoaFilter filtro = new PessoaFilter();
+
+	private LazyDataModel<Pessoa> listaFuncionarios;
+
+	@Inject
+	private PessoaService funcionarioService;
+
+	/******************** Metodos ***********************/
+
+	public void inicializar() {
+		if (this.funcionario == null) {
+			novo();
+		}
+		pesquisar();
+	}
+
+	public void salvar() {
+
+		Pessoa funcionarioExistente = funcionarioService.porCpf(funcionario.getCpfCnpj());
+		if (funcionarioExistente != null && !funcionarioExistente.equals(funcionario)) {
+			throw new NegocioException("Já existe um Funcionário com esse cpf ou cnpj informado.");
+		}
+
+		RequestContext request = RequestContext.getCurrentInstance();
+		request.addCallbackParam("sucesso", true);
+		funcionarioService.salvar(funcionario);
+		Messages.addGlobalInfo("Registro salvor com sucesso.");
+		pesquisar();
+	}
+
+	public void editar() {
+		this.funcionario = funcionarioService.porId(this.funcionarioSelecionado.getId());
+	}
+
+	public void novo() {
+		funcionario = new Pessoa();
+		funcionario.setFuncionario(true);
+	}
+
+	public void novoFiltro() {
+		this.filtro = new PessoaFilter();
+	}
+
+	public void pesquisar() {
+
+		listaFuncionarios = new LazyDataModel<Pessoa>() {
+
+			private static final long serialVersionUID = 1L;
+
+			public List<Pessoa> load(int first, int pageSize, String sortField, SortOrder sortOrder,
+					Map<String, Object> filters) {
+
+				filtro.setPrimeiroRegistro(first);
+				filtro.setQuantidadeRegistros(pageSize);
+				filtro.setAscendente(SortOrder.ASCENDING.equals(sortOrder));
+				filtro.setCampoOrdenacao(sortField);
+				filtro.setFuncionario(true);
+
+				setRowCount(funcionarioService.quantidadeFiltrados(filtro));
+
+				return funcionarioService.filtrados(filtro);
+			}
+
+			@Override
+			public Pessoa getRowData(String rowKey) {
+				Pessoa objeto = (Pessoa) funcionarioService.porId(Long.parseLong(rowKey));
+				return objeto;
+			}
+
+			@Override
+			public String getRowKey(Pessoa objeto) {
+				return objeto.getId().toString();
+			}
+
+		};
+	}
+
+	public void preparEdicao() {
+		this.funcionario = funcionarioService.porId(this.funcionarioSelecionado.getId());
+	}
+
+	public void excluir() {
+		funcionarioService.excluir(funcionarioSelecionado);
+		Messages.addGlobalInfo("Registro excluido com sucesso.");
+		pesquisar();
+	}
+
+	public List<TipoPessoa> getlistaTipoPessoas() {
+		return Arrays.asList(TipoPessoa.values());
+	}
+
+	public List<Sexo> getlistaSexos() {
+		return Arrays.asList(Sexo.values());
+	}
+
+	public List<EstadoCivil> getlistaEstadoCivis() {
+		return Arrays.asList(EstadoCivil.values());
+	}
+
+	public List<Estado> getlistaEstados() {
+		return Arrays.asList(Estado.values());
+	}
+
+	/******************** Getters e Setters ***************************/
+
+	public PessoaFilter getFiltro() {
+		return filtro;
+	}
+
+	public Pessoa getFuncionario() {
+		return funcionario;
+	}
+
+	public void setFuncionario(Pessoa funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	public void setFiltro(PessoaFilter filtro) {
+		this.filtro = filtro;
+	}
+
+	public LazyDataModel<Pessoa> getListaFuncionarios() {
+		return listaFuncionarios;
+	}
+
+	public void setListaFuncionarios(LazyDataModel<Pessoa> listaFuncionarios) {
+		this.listaFuncionarios = listaFuncionarios;
+	}
+
+	public Pessoa getFuncionarioSelecionado() {
+		return funcionarioSelecionado;
+	}
+
+	public void setFuncionarioSelecionado(Pessoa funcionarioSelecionado) {
+		this.funcionarioSelecionado = funcionarioSelecionado;
+	}
+
+}
--- src/main/java/com/br/apss/drogaria/model/Pessoa.java
+++ src/main/java/com/br/apss/drogaria/model/Pessoa.java
@@ -1,6 +1,7 @@
 package com.br.apss.drogaria.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -128,6 +129,132 @@ public class Pessoa implements Serializable {
 
 	@Column(name = "pai", length = 80)
 	private String pai;
+
+	@Column(name = "num_doc", length = 25)
+	private String numDoc;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_emissao", length = 10)
+	private Date dataEmissao;
+
+	@Column(name = "orgao_emissor", length = 25)
+	private String orgaoEmissor;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "uf_emissor", length = 2)
+	private Estado ufEmissor;
+
+	@Column(name = "num_seguranca", length = 25)
+	private String num_seguranca;
+
+	@Column(name = "ocupacao", length = 80)
+	private String ocupacao;
+
+	@Column(name = "nome_empresa", length = 80)
+	private String nomeEmpresa;
+
+	@Column(name = "emp_telefone", length = 20)
+	private String empTelefone;
+
+	@Column(name = "emp_cep", length = 10)
+	private String empCep;
+
+	@Column(name = "emp_endereco", length = 80)
+	private String empEndereco;
+
+	@Column(name = "emp_num", length = 10)
+	private String empNumero;
+
+	@Column(name = "emp_complemento", length = 80)
+	private String empComplemento;
+
+	@Column(name = "emp_bairro", length = 80)
+	private String empBairro;
+
+	@Column(name = "emp_uf", length = 2)
+	@Enumerated(EnumType.STRING)
+	private Estado empUf;
+
+	@Column(name = "emp_cidade", length = 80)
+	private String empCidade;
+
+	@Column(name = "nome_banco", length = 80)
+	private String banco;
+
+	@Column(name = "tipo_conta", length = 80)
+	private String tipoConta;
+
+	@Column(name = "agencia", length = 10)
+	private String agencia;
+
+	@Column(name = "dig_agencia", length = 5)
+	private String digAgencia;
+
+	@Column(name = "num_conta", length = 30)
+	private String numConta;
+
+	@Column(name = "dig_conta", length = 5)
+	private String digConta;
+
+	@Column(name = "conta_conjunta", length = 3)
+	private Boolean contaConjunta;
+
+	@Column(name = "renda", precision = 12, scale = 2)
+	private BigDecimal renda;
+
+	@Column(name = "vlr_ultima_compra", precision = 12, scale = 2)
+	private BigDecimal vlrUltimaCompra;
+
+	@Column(name = "limite", precision = 12, scale = 2)
+	private BigDecimal limite;
+
+	@Column(name = "venda_aprazo", length = 1)
+	private Boolean vendaAprazo;
+
+	@Column(name = "bloquear_vencto", length = 1)
+	private Boolean bloquearVenco;
+
+	@Column(name = "motivo_bloqueio", length = 40)
+	private String motivoBloqueio;
+
+	@Column(name = "titulo_eleitor", length = 20)
+	private String tituloEleitor;
+
+	@Column(name = "zona", length = 10)
+	private String zona;
+
+	@Column(name = "secao", length = 10)
+	private String secao;
+
+	@Column(name = "cateira_reservista", length = 25)
+	private String cateiraReservista;
+
+	@Column(name = "ctps", length = 20)
+	private String ctps;
+
+	@Column(name = "serie_ctps", length = 10)
+	private String serieCtps;
+
+	@Column(name = "uf_ctps", length = 2)
+	private String ufCtps;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "emissao_ctps", length = 10)
+	private Date emissaoCtps;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "admissao", length = 10)
+	private Date admissao;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "demissao", length = 10)
+	private Date demissao;
+
+	@Column(name = "pis_pasep", length = 20)
+	private String pisPasep;
+
+	@Column(name = "trabalha", length = 1)
+	private Boolean trabalha;
 
 	public Long getId() {
 		return id;
@@ -383,6 +510,326 @@ public class Pessoa implements Serializable {
 
 	public void setContato2(String contato2) {
 		this.contato2 = contato2 == null ? null : contato2.toUpperCase();
+	}
+
+	public String getNumDoc() {
+		return numDoc;
+	}
+
+	public void setNumDoc(String numDoc) {
+		this.numDoc = numDoc;
+	}
+
+	public Date getDataEmissao() {
+		return dataEmissao;
+	}
+
+	public void setDataEmissao(Date dataEmissao) {
+		this.dataEmissao = dataEmissao;
+	}
+
+	public String getOrgaoEmissor() {
+		return orgaoEmissor;
+	}
+
+	public void setOrgaoEmissor(String orgaoEmissor) {
+		this.orgaoEmissor = orgaoEmissor;
+	}
+
+	public Estado getUfEmissor() {
+		return ufEmissor;
+	}
+
+	public void setUfEmissor(Estado ufEmissor) {
+		this.ufEmissor = ufEmissor;
+	}
+
+	public String getNum_seguranca() {
+		return num_seguranca;
+	}
+
+	public void setNum_seguranca(String num_seguranca) {
+		this.num_seguranca = num_seguranca;
+	}
+
+	public String getOcupacao() {
+		return ocupacao;
+	}
+
+	public void setOcupacao(String ocupacao) {
+		this.ocupacao = ocupacao;
+	}
+
+	public String getNomeEmpresa() {
+		return nomeEmpresa;
+	}
+
+	public void setNomeEmpresa(String nomeEmpresa) {
+		this.nomeEmpresa = nomeEmpresa;
+	}
+
+	public String getEmpTelefone() {
+		return empTelefone;
+	}
+
+	public void setEmpTelefone(String empTelefone) {
+		this.empTelefone = empTelefone;
+	}
+
+	public String getEmpCep() {
+		return empCep;
+	}
+
+	public void setEmpCep(String empCep) {
+		this.empCep = empCep;
+	}
+
+	public String getEmpEndereco() {
+		return empEndereco;
+	}
+
+	public void setEmpEndereco(String empEndereco) {
+		this.empEndereco = empEndereco;
+	}
+
+	public String getEmpNumero() {
+		return empNumero;
+	}
+
+	public void setEmpNumero(String empNumero) {
+		this.empNumero = empNumero;
+	}
+
+	public String getEmpComplemento() {
+		return empComplemento;
+	}
+
+	public void setEmpComplemento(String empComplemento) {
+		this.empComplemento = empComplemento;
+	}
+
+	public String getEmpBairro() {
+		return empBairro;
+	}
+
+	public void setEmpBairro(String empBairro) {
+		this.empBairro = empBairro;
+	}
+
+	public Estado getEmpUf() {
+		return empUf;
+	}
+
+	public void setEmpUf(Estado empUf) {
+		this.empUf = empUf;
+	}
+
+	public String getEmpCidade() {
+		return empCidade;
+	}
+
+	public void setEmpCidade(String empCidade) {
+		this.empCidade = empCidade;
+	}
+
+	public String getBanco() {
+		return banco;
+	}
+
+	public void setBanco(String banco) {
+		this.banco = banco;
+	}
+
+	public String getTipoConta() {
+		return tipoConta;
+	}
+
+	public void setTipoConta(String tipoConta) {
+		this.tipoConta = tipoConta;
+	}
+
+	public String getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(String agencia) {
+		this.agencia = agencia;
+	}
+
+	public String getDigAgencia() {
+		return digAgencia;
+	}
+
+	public void setDigAgencia(String digAgencia) {
+		this.digAgencia = digAgencia;
+	}
+
+	public String getNumConta() {
+		return numConta;
+	}
+
+	public void setNumConta(String numConta) {
+		this.numConta = numConta;
+	}
+
+	public String getDigConta() {
+		return digConta;
+	}
+
+	public void setDigConta(String digConta) {
+		this.digConta = digConta;
+	}
+
+	public Boolean getContaConjunta() {
+		return contaConjunta;
+	}
+
+	public void setContaConjunta(Boolean contaConjunta) {
+		this.contaConjunta = contaConjunta;
+	}
+
+	public BigDecimal getRenda() {
+		return renda;
+	}
+
+	public void setRenda(BigDecimal renda) {
+		this.renda = renda;
+	}
+
+	public BigDecimal getVlrUltimaCompra() {
+		return vlrUltimaCompra;
+	}
+
+	public void setVlrUltimaCompra(BigDecimal vlrUltimaCompra) {
+		this.vlrUltimaCompra = vlrUltimaCompra;
+	}
+
+	public BigDecimal getLimite() {
+		return limite;
+	}
+
+	public void setLimite(BigDecimal limite) {
+		this.limite = limite;
+	}
+
+	public Boolean getVendaAprazo() {
+		return vendaAprazo;
+	}
+
+	public void setVendaAprazo(Boolean vendaAprazo) {
+		this.vendaAprazo = vendaAprazo;
+	}
+
+	public Boolean getBloquearVenco() {
+		return bloquearVenco;
+	}
+
+	public void setBloquearVenco(Boolean bloquearVenco) {
+		this.bloquearVenco = bloquearVenco;
+	}
+
+	public String getMotivoBloqueio() {
+		return motivoBloqueio;
+	}
+
+	public void setMotivoBloqueio(String motivoBloqueio) {
+		this.motivoBloqueio = motivoBloqueio;
+	}
+
+	public String getTituloEleitor() {
+		return tituloEleitor;
+	}
+
+	public void setTituloEleitor(String tituloEleitor) {
+		this.tituloEleitor = tituloEleitor;
+	}
+
+	public String getZona() {
+		return zona;
+	}
+
+	public void setZona(String zona) {
+		this.zona = zona;
+	}
+
+	public String getSecao() {
+		return secao;
+	}
+
+	public void setSecao(String secao) {
+		this.secao = secao;
+	}
+
+	public String getCateiraReservista() {
+		return cateiraReservista;
+	}
+
+	public void setCateiraReservista(String cateiraReservista) {
+		this.cateiraReservista = cateiraReservista;
+	}
+
+	public String getCtps() {
+		return ctps;
+	}
+
+	public void setCtps(String ctps) {
+		this.ctps = ctps;
+	}
+
+	public String getSerieCtps() {
+		return serieCtps;
+	}
+
+	public void setSerieCtps(String serieCtps) {
+		this.serieCtps = serieCtps;
+	}
+
+	public String getUfCtps() {
+		return ufCtps;
+	}
+
+	public void setUfCtps(String ufCtps) {
+		this.ufCtps = ufCtps;
+	}
+
+	public Date getEmissaoCtps() {
+		return emissaoCtps;
+	}
+
+	public void setEmissaoCtps(Date emissaoCtps) {
+		this.emissaoCtps = emissaoCtps;
+	}
+
+	public Date getAdmissao() {
+		return admissao;
+	}
+
+	public void setAdmissao(Date admissao) {
+		this.admissao = admissao;
+	}
+
+	public Date getDemissao() {
+		return demissao;
+	}
+
+	public void setDemissao(Date demissao) {
+		this.demissao = demissao;
+	}
+
+	public String getPisPasep() {
+		return pisPasep;
+	}
+
+	public void setPisPasep(String pisPasep) {
+		this.pisPasep = pisPasep;
+	}
+
+	public Boolean getTrabalha() {
+		return trabalha;
+	}
+
+	public void setTrabalha(Boolean trabalha) {
+		this.trabalha = trabalha;
 	}
 
 	public boolean isInclusao() {
